e3da9f5000b1a2064ca734cfa49ec41152d1eb40
Vivek Kumar Pandey <vivmoney13@hotmail.com>
2021-03-31 01:31:29 +0530

    Showing correct answer count

--- Scripts/NGQuiz.js
+++ Scripts/NGQuiz.js
@@ -1,7 +1,9 @@
 var app = angular.module('myApp', []);
 app.controller('quizCtrl', function ($scope, $http) {
     $scope.results = [];
+    $scope.ans = [];
     $scope.quizSubmitted = false;
+    $scope.correctAnswerCount = 0;
     $scope.getData = function () {
 
         var url = 'https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=multiple';
@@ -15,9 +17,13 @@ app.controller('quizCtrl', function ($scope, $http) {
             });
     }
 
-    $scope.validate = function (formData) {
+    $scope.validate = function () {
         $scope.quizSubmitted = true;
-        $scope.quizResult = "You clicked submit!";
+        $scope.results.forEach(function(item, index){
+            if($scope.ans[index].includes(item.correct_answer)){
+                $scope.correctAnswerCount++;
+            }
+        });
         
     }
 });

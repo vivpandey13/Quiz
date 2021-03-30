ea53c95a5942c12b6b103e80ef67293016d481c9
vivpandey13 <44554220+vivpandey13@users.noreply.github.com>
2021-03-31 01:34:54 +0530

    Update NGQuiz.js

--- Scripts/NGQuiz.js
+++ Scripts/NGQuiz.js
@@ -1,13 +1,29 @@
 var app = angular.module('myApp', []);
 app.controller('quizCtrl', function ($scope, $http) {
     $scope.results = [];
-    var url = 'https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=multiple';
-    $http.get(url)
-        .then(function (response) {
-            $scope.results = response.data.results;
-            $scope.results.forEach(element => {
-                element.answers = element.incorrect_answers;
-                element.answers.push(element.correct_answer);
+    $scope.ans = [];
+    $scope.quizSubmitted = false;
+    $scope.correctAnswerCount = 0;
+    $scope.getData = function () {
+
+        var url = 'https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=multiple';
+        $http.get(url)
+            .then(function (response) {
+                $scope.results = response.data.results;
+                $scope.results.forEach(element => {
+                    element.answers = element.incorrect_answers;
+                    element.answers.push(element.correct_answer);
+                });
             });
+    }
+
+    $scope.validate = function () {
+        $scope.quizSubmitted = true;
+        $scope.results.forEach(function(item, index){
+            if($scope.ans[index].includes(item.correct_answer)){
+                $scope.correctAnswerCount++;
+            }
         });
+        
+    }
 });

2a0977f8bff7977c0bcf60cd8f6ac2d2fdd63fbb
Vivek Kumar Pandey <vivmoney13@hotmail.com>
2021-03-31 00:04:23 +0530

    Form submit initial changes

--- Scripts/NGQuiz.js
+++ Scripts/NGQuiz.js
@@ -1,13 +1,23 @@
 var app = angular.module('myApp', []);
 app.controller('quizCtrl', function ($scope, $http) {
     $scope.results = [];
-    var url = 'https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=multiple';
-    $http.get(url)
-        .then(function (response) {
-            $scope.results = response.data.results;
-            $scope.results.forEach(element => {
-                element.answers = element.incorrect_answers;
-                element.answers.push(element.correct_answer);
+    $scope.quizSubmitted = false;
+    $scope.getData = function () {
+
+        var url = 'https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=multiple';
+        $http.get(url)
+            .then(function (response) {
+                $scope.results = response.data.results;
+                $scope.results.forEach(element => {
+                    element.answers = element.incorrect_answers;
+                    element.answers.push(element.correct_answer);
+                });
             });
-        });
+    }
+
+    $scope.validate = function (formData) {
+        $scope.quizSubmitted = true;
+        $scope.quizResult = "You clicked submit!";
+        
+    }
 });
